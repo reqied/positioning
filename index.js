7f38408275aedc1d65d7c1577fdb45903d226214
Ivan-Malgin <i.v.malgin2006@gmail.com>
2026-04-07 19:28:17 +0500

    p2

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+
+function openModal() {
+    modal.style.display = "flex"; 
+    setTimeout(() => {
+        modal.classList.add("show");
+    }, 10);
+}
+
+function closeModal() {
+    modal.classList.remove("show");
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300);
+}
+btn.onclick = openModal;
+
+span.onclick = closeModal;
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
